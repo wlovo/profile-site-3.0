d0a57de37aee0a3d240679208946d032c49f69e0
Will Lovo <will9637@gmail.com>
2025-06-18 21:13:50 -0400

    Refactor layout and intro components

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,6 +3,7 @@
 import CodeBlock from '@/components/code-block';
 import moment from 'moment';
 import { line, simpleLine } from '@/lib/utils';
+import React from 'react';
 
 const experienceDuration = moment.duration(moment().diff(moment('2019-06-01')));
 
@@ -10,24 +11,31 @@ const codeLines = [
   simpleLine('{'),
 
   // Name
-  line(['name: ', ["'William H. Lovo',", 'text-blue-300']], 1),
+  line(['name: ', ["'William H. Lovo',", 'text-sky-500']], 1),
 
   // Experience
   simpleLine('experience: {', 1),
   line(
     [
       'time: ',
-      [`'${experienceDuration.years()} years and ${experienceDuration.months()} months'`, 'text-purple-400'],
+      [`'${experienceDuration.years()} years and ${experienceDuration.months()} months'`, 'text-purple-500'],
       ',',
     ],
     2
   ),
   simpleLine('current: {', 2),
+  line(['company: ', ["'Capital One'", 'text-blue-600'], ','], 3),
+  line(['title: ', ["'Senior Software Engineer'", 'text-blue-600'], ','], 3),
   simpleLine('},', 2),
   simpleLine('},', 1),
   simpleLine('}'),
 ];
 
 export default function Intro() {
-  return <CodeBlock codeLines={codeLines} activeFileName="intro" inactiveFiles={['next-prospect']} />;
+  return (
+    <React.Fragment>
+      <CodeBlock codeLines={codeLines} activeFileName="intro" inactiveFiles={['next-prospect']} />
+      <div></div>
+    </React.Fragment>
+  );
 }
