c2711ec9737d09281c1aad0262fbb2f56cdf8022
Will Lovo <will9637@gmail.com>
2026-03-02 01:15:50 -0500

    dynamic year for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div id="footer" className="flex items-center justify-center p-4 bg-slate-100 dark:bg-slate-900">
-      <p className="text-sm text-gray-600 dark:text-gray-400">© 2025 William Lovo</p>
+      <p className="text-sm text-gray-600 dark:text-gray-400">© {new Date().getFullYear()} William Lovo</p>
     </div>
   );
 }
